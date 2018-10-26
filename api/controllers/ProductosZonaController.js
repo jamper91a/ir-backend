9398bfc84e1c4df5f102fbaa6e62128e25746be5
Jorge Andrés Moreno Jaimes <jamper91a@gmail.com>
2018-10-27 10:42:17 +1300

    Se implementa el generador de respuestas estandar

--- api/controllers/ProductosZonaController.js
+++ api/controllers/ProductosZonaController.js
@@ -4,60 +4,20 @@
  * @description :: Server-side logic for managing ProductosZona
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+const name="Productos Zona";
 module.exports = {
-    index: function(req, res, next) {
-        ProductosZona.find().exec(function(err, list) {
-            if (err) return Error('Error');
-            return res.view({
-                result: list
-            });
-        });
-    },
 
-    show: function(req, res, next) {
-        ProductosZona.findOneById(req.param('id'), function Founded(err, value) {
-            if (err) {
-                return next(err);
-            }
-            res.view({
-                element: value
-            });
-        });
-    },
-
-    edit: function(req, res, next) {
-        ProductosZona.findOne(req.param('id'), function Founded(err, value) {
-            if (err) {
-                return next(err);
-            }
-            res.view({
-                element: value
-            });
-        });
-    },
-
-    update: function(req, res, next) {
-        ProductosZona.update(req.param('id'), req.body, function Update(err, value) {
-            if (err) {
-                return next(err);
-            }
-            return res.redirect('productosZona/show/' + req.param('id'));
-        });
-    },
-
-    delete: function(req, res, next) {
-        ProductosZona.destroy(req.param('id'), function Update(err, value) {
-            if (err) {
-                return next(err);
-            }
-            return res.redirect('/productosZona');
-        });
-    },
-
-    addMercancia:function (req, res, next) {
+    addMercancia:function (req, res) {
       //Validate data
       if(!req.body.productos_zona || !req.body.productos_id){
-        return res.serverError("Datos no validos");
+        var things={
+          code: 'error_G01',
+          req:req,
+          res:res,
+          data:[],
+          error:null
+        };
+          return res.generalAnswer(things);
       }
 
       var productos_zona = JSON.parse(req.body.productos_zona);
@@ -69,23 +29,21 @@ module.exports = {
             ProductosZona.create(producto_zona).usingConnection(db).fetch().then(function (aux) {
               cb();
             }).catch(function (err) {
-              console.error(err.error);
-              cb(new Error("Incoveniente al crear los registros de dispositivo y producto: "+producto_zona.epcs_id));
-
-              //return null;
+              var things={code: 'error_PZ01', req:req, res:res, data:[], error:err};
+              cb(things);
             });
           }, function (error) {
             if(error)
             {
-              return proceed(error);
+              return proceed(null,error);
             }else{
               let product = Productos.find({
                   id: req.body.productos_id
               }).limit(1).usingConnection(db).then(function (product) {
                 return product;
               }).catch(function (err) {
-                return proceed(new Error("Incoveniente al obtener el producto"));
-                //return null;
+                var things={code: 'error_P01', req:req, res:res, data:[], error:err};
+                return proceed(null,things);
               });
               //Actualizo los devices
               var epcs = Epcs.update(_.map(productos_zona, 'epcs_id'), {state: 1}).usingConnection(db).fetch().then(function (devices) {
@@ -94,8 +52,8 @@ module.exports = {
                 else
                   return [];
               }).catch(function (err) {
-                return proceed(new Error("Incoveniente al crear los registros de dispositivos"));
-                //return null;
+                var things={code: 'error_EPC02', req:req, res:res, data:[], error:err};
+                return proceed(null,things);
               });
 
               Promise.all([
@@ -103,16 +61,16 @@ module.exports = {
                 epcs
               ]).then(function (values) {
                 if(values){
-                  return proceed(null,{
-                    productos: values[0],
-                    epcs: values[1]
-                  });
+                  var things={code: '', req:req, res:res, data:values[1], error:null};
+                  return proceed(null,things);
                 }else {
-                  return proceed(new Error("Resultados invalidos"));
+                  var things={code: 'error_G05', req:req, res:res, data:[], error:null};
+                  return proceed(null, things);
                 }
 
               }).catch(function (err) {
-                return proceed(new Error(err.message));
+                var things={code: '', req:req, res:res, data:[], error:err};
+                return proceed(null,things);
               });
             }
 
@@ -124,10 +82,10 @@ module.exports = {
 
 
         }).then(function (operation) {
-        return res.json(operation);
+        return res.generalAnswer(operation);
 
       }).catch(function (error) {
-        return res.serverError(error.message);
+        return res.generalAnswer(error);
       });
     }
 
