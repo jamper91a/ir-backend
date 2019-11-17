4a74e0589a14804ffac138fa8204a07a8c123c43
jamper91a <jamper91a@gmail.com>
2019-11-17 13:24:09 -0500

    Create shop

--- back-end/api/controllers/ShopsController.js
+++ back-end/api/controllers/ShopsController.js
@@ -6,5 +6,18 @@
  */
 module.exports = {
 
-
+  create: async function (req, res) {
+    let things;
+    const company = await Companies.findOne({user: req.user.id});
+    try {
+      req.body.company = company.id;
+      await Shops.create(req.body);
+      things = {code: '', data: {}, error: null, propio: false, bd: false};
+      return res.generalAnswer(things);
+    } catch (e) {
+      console.error(e);
+      things = {code: '', data: [], error: e};
+      return res.generalAnswer(things);
+    }
+  }
 };
